0b571dd94f9d63d3ee1f336e826eec67ba743c6a
druvkotwani <druvkotwani12@gmail.com>
2023-10-24 17:55:13 +0530

    png

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,6 @@ import { AiFillCheckCircle } from 'react-icons/ai';
 
 
 const Card = ({ background, shadow, text, price }) => {
-    console.log(background, shadow)
     return (
         <div className={`flex flex-col items-center w-[360px] h-[576px] py-8 px-4 justify-between rounded-[20px] ${background}`} style={{ boxShadow: `0px 0px 100px 0px ${shadow}` }}>
             <header className='flex flex-col items-start gap-6'>
